187269618761c50e3b04c4cf28bc55aab8c9fae9
Islahihyaa <islah.ihya@gmail.com>
2024-03-11 21:44:14 +0700

    Fixed pages

--- components/table.php
+++ components/table.php
@@ -2,33 +2,35 @@
     require __DIR__ . '/../api/get_standings.php';
 ?>
 
-
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">Position</th>
-            <th scope="col">Club</th>
-            <th scope="col">Play</th>
-            <th scope="col">Win</th>
-            <th scope="col">Draw</th>
-            <th scope="col">Lose</th>
-            <th scope="col">Point</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($response as $country): ?>
-            <tr>
-                <td><?php echo $country['overall_league_position']; ?></td>
-                <td>
-                    <img src="<?php echo $country['team_badge']; ?>" alt="Logo team">
-                    <?php echo $country['team_name']; ?>
-                </td>
-                <td><?php echo $country['overall_league_payed']; ?></td>
-                <td><?php echo $country['overall_league_W']; ?></td>
-                <td><?php echo $country['overall_league_D']; ?></td>
-                <td><?php echo $country['overall_league_L']; ?></td>
-                <td><?php echo $country['overall_league_PTS']; ?></td>
+<div class="bg-white mt-14">
+    <table class="table table-bordered">
+        <thead class="table table-primary">
+            <tr class="text-center">
+                <th scope="col">Position</th>
+                <th scope="col">Club</th>
+                <th scope="col">Play</th>
+                <th scope="col">Win</th>
+                <th scope="col">Draw</th>
+                <th scope="col">Lose</th>
+                <th scope="col">Point</th>
             </tr>
-        <?php endforeach; ?>            
-    </tbody>
+        </thead>
+        <tbody>
+            <?php foreach ($response as $country): ?>
+                <tr>
+                    <td class="text-center"><?php echo $country['overall_league_position']; ?></td>
+                    <td>
+                        <img src="<?php echo $country['team_badge']; ?>" alt="Logo team" width="50px">
+                        <?php echo $country['team_name']; ?>
+                    
+                    </td>
+                    <td class="text-center"><?php echo $country['overall_league_payed']; ?></td>
+                    <td class="text-center"><?php echo $country['overall_league_W']; ?></td>
+                    <td class="text-center"><?php echo $country['overall_league_D']; ?></td>
+                    <td class="text-center"><?php echo $country['overall_league_L']; ?></td>
+                    <td class="text-center"><?php echo $country['overall_league_PTS']; ?></td>
+                </tr>
+            <?php endforeach; ?>            
+        </tbody>
     </table>
+</div>
--- pages/indonesiaLeague.php
+++ pages/indonesiaLeague.php
@@ -1,10 +1,10 @@
 <?php
-    $id_league = 168; 
+    $id_league = 194; 
 ?>
 
 <div class="card mt-14">
   <div class="card-body">
-    <h5 class="card-title fs-1">France</h5>
+    <h5 class="card-title fs-1">Indonesia</h5>
     <h6 class="card-subtitle mb-2 text-body-secondary fs-4">League</h6>
     <?php
         include __DIR__ . '/../components/table.php';
